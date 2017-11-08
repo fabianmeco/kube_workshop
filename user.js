69b0f650dfa43c525a86d0c218d4611354d2d03c
Fabian Mendoza <yessid.mendoza@gmail.com>
2017-11-08 07:26:04 -0500

    update array

--- user.js
+++ user.js
@@ -1,28 +1,51 @@
 var express = require('express');
 var route = express.Router();
+var arr = [];
 
 route.use(function timeLog (req, res, next) {
     console.log('Time: ', Date.now())
     next()
   })
 
-route.post('/', function(req, res){
-    res.send('new user'+req.body.name)
+route.post('/:userId/:username', function(req, res){
+    var user = {"userId":req.params.userId, "username":req.params.username};
+    arr.push(user);
+    res.send('new user: '+arr[arr.length-1].userId+" with name: "+arr[arr.length-1].username)
 });
 
 
 route.get('/', function(req, res){
-    res.send("all users")
+    var msg = "";
+    arr.forEach(function(value){
+        msg += value.userId +", "+ value.username + " - ";
+    });
+    res.send("Users:" + msg)
 });
 
 route.get('/:userId', function(req, res){
-    res.send("user: "+req.params.userId)
+    var msg = "";
+    arr.forEach(function(value){
+        if(value.userId===req.params.userId){
+            msg+= value.userId +", "+ value.username + " - ";
+        }
+    });
+    res.send("user: "+msg)
 });
 
 route.delete('/:userId', function(req, res){
+    arr.forEach(function(value){
+        if(value.userId===req.params.userId){
+            arr.splice(value.indexOf(),1);
+        }        
+    });
     res.send('you killed this user '+req.params.userId+' :(')
 });
-route.put('/:userId', function(req, res){
+route.put('/:userId/:newname', function(req, res){
+    arr.forEach(function(value){
+        if(value.userId===req.params.userId){
+            value.username = req.params.newname;
+        }       
+    });
     res.send('the user '+req.params.userId+' has been updated (cyborg voice)')
 });
 
