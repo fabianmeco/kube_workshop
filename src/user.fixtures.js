9662af2c6ece64c52a47b28e033718b78e8989be
Fabian Mendoza <yessid.mendoza@gmail.com>
2017-11-09 22:31:26 -0500

    With problem

--- src/user.fixtures.js
+++ src/user.fixtures.js
@@ -4,8 +4,8 @@ module.exports = {
             "username":"Yay",
             "first_name":"Clara",
             "last_name":"Oswald",
-            "phone": 123456789005654,
-            "email": "algo@algomas.alguito",
+            "phone": 1234567890,
+            "email": "algo@algomas.alg",
             "gender": "F"
         }    
     },
